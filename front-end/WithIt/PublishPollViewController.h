74c366a6c8a13a7003fd29018d9ce096701161ba
peggytan <peggytan@usc.edu>
2014-01-22 20:41:07 -0800

    Add PublishPollViewController

---
+++ front-end/WithIt/PublishPollViewController.h
@@ -0,0 +1,13 @@
+//
+//  PublishPollViewController.h
+//  WithIt
+//
+//  Created by Peggy Tang on 22/1/14.
+//  Copyright (c) 2014 WithIt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PublishPollViewController : UIViewController
+
+@end
---
+++ front-end/WithIt/PublishPollViewController.m
@@ -0,0 +1,39 @@
+//
+//  PublishPollViewController.m
+//  WithIt
+//
+//  Created by Peggy Tang on 22/1/14.
+//  Copyright (c) 2014 WithIt. All rights reserved.
+//
+
+#import "PublishPollViewController.h"
+
+@interface PublishPollViewController ()
+
+@end
+
+@implementation PublishPollViewController
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
